dc319feaeaf0966c75048dc4a1d14b7583f78768
Viktor Zetterström <viktor.zetterstrom@footway.com>
2019-11-29 13:21:37 +0100

    remove cors handling

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
 const express = require('express');
-const cors = require('cors');
 const trivia = require('./trivia');
 
 const app = express();
 app.use(require('helmet')());
 
-const whitelist = ['https://trivia.zetterstrom.dev'];
 
-const corsOptions = {
-  origin: (origin, cb) => {
-    if (whitelist.includes(origin) || process.env.NODE_ENV === 'development') {
-      cb(null, true);
-    } else {
-      cb(new Error('Not allowed by CORS'));
-    }
-  },
-};
-
-app.get('/questions', cors(corsOptions), async (req, res) => {
+app.get('/questions', async (req, res) => {
   const questions = await trivia.service.questions(req.query);
   res.json(questions);
 });
